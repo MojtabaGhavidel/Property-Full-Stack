3393117661c30c5a7b800a479bf53daff7724737
Mojtaba Ghavidel <mojtabaghavidel@gmail.com>
2016-12-17 16:54:44 -0800

    implement basic index

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css','../../assets/css/jquery.selectBox.css','../../assets/css/jquery-ui.css','../../assets/css/font-awesome.min.css','../../assets/css/style.css','../../assets/css/responsive.css','../../assets/css/pace-minimal.css',]
 })
 export class AboutComponent {
 
